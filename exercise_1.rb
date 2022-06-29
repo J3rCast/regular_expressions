de412b0d691e0bcb58359114719a764986692ac0
Coderlancce <coderlancce@gmail.com>
2022-06-29 14:08:37 -0500

    [OK] email

--- exercise_1.rb
+++ exercise_1.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/^([a-zA-Z0-9_%+-]+@[a-zA-Z0-9_%+-]+\.[a-z]+{1,2})(\.[a-z]+)?$/).join
-puts ARGV[1].scan(/^(?=.*\W)(?=.*[a-z]).{3,}(?=.*[A-Z]).{3,}(?=.*[0-9])?$/).join
+puts ARGV[0].scan(/^([a-zA-Z0-9._%+-]+@[a-zA-Z0-9_%+-]+\.[a-z]+{1,2})(\.[a-z]+)?$/).join
+puts ARGV[1].scan(/^(?=.*[a-zA-Z]{3,})(?=.*[0-9\W])?.{7,}$/).join
